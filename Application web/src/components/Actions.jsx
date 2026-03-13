65706dcd10eaeec7897a1f9902a8f13343c62856
DrunkAxolotl <aresgabrielyan@gmail.com>
2026-03-13 19:04:22 -0400

    Fixed Actions section html (part 3)

--- Application web/src/components/Actions.jsx
+++ Application web/src/components/Actions.jsx
@@ -1,21 +1,18 @@
 
 
-/** Actions */
+// Actions
 export default function Actions(){
   return(
 
-      /** Actions layout
-       * TO CHANGE DYNAMICALLY LATER
-        */
-    <main class="Actions">
+    // Actions layout
+    <div class="Actions">
         Actions
-        // onCreerClick to be handled later
         <button type="button" onClick="onCreerClick">Créer</button>
         <menu>
-            <li>Exemple 1</li>
-            <li>Exemple 2</li>
-            <li>Exemple 3</li>
+            <ul>Exemple 1</ul>
+            <ul>Exemple 2</ul>
+            <ul>Exemple 3</ul>
         </menu>
-    </main>
+    </div>
   );
 }
