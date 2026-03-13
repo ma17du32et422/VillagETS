a3b23a6465cbc2854c4c20f1409dd17c0e9d0158
DrunkAxolotl <aresgabrielyan@gmail.com>
2026-03-13 18:53:02 -0400

    Added Actions section html

--- Application web/src/components/Actions.jsx
+++ Application web/src/components/Actions.jsx
@@ -1,8 +1,21 @@
+
+
 /** Actions */
 export default function Actions(){
   return(
-    <div>
-      Actions 
-    </div>
+
+      /** Actions layout
+       * TO CHANGE DYNAMICALLY LATER
+        */
+    <main class="Actions">
+        Actions
+        // onCreerClick to be handled later
+        <button type="button" onClick="onCreerClick">Créer</button>
+        <menu>
+            <li>Exemple 1</li>
+            <li>Exemple 2</li>
+            <li>Exemple 3</li>
+        </menu>
+    </main>
   );
 }
