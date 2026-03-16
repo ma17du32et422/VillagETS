c82ced8499e4e80487abc950551b03bceca35717
LoicBeaudin <beaudinkerzerho@outlook.com>
2026-03-16 09:31:31 -0400

    ajout drawer+ image

--- VillagETS_AndroidStudio/app/src/main/java/com/example/villagets_androidstudio/MainActivity.java
+++ VillagETS_AndroidStudio/app/src/main/java/com/example/villagets_androidstudio/MainActivity.java
@@ -1,12 +1,15 @@
 package com.example.villagets_androidstudio;
 
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
+import androidx.core.view.GravityCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        ImageButton menuBtn = findViewById(R.id.menuBtn);
+
+        menuBtn.setOnClickListener(v -> {
+            drawerLayout.openDrawer(GravityCompat.START);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
