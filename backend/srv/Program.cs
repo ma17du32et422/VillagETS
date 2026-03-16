c935fb2e1e98e885bc3f8b4bcde127498623b440
Guarded0 <ricardoarb007@gmail.com>
2026-03-16 14:29:29 -0400

    backend + gitignore
    succesful communication with sql

--- backend/srv/Program.cs
+++ backend/srv/Program.cs
@@ -1,26 +1,39 @@
-using Microsoft.Extensions.FileProviders;
-using Microsoft.Data.SqlClient;
-
+using Supabase;
+using Supabase.Postgrest.Attributes;
+using Supabase.Postgrest.Models;
+using System.Text.Json;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello world");
-
-app.MapGet("/test-db", async () =>
+var url = Environment.GetEnvironmentVariable("SUPABASE_URL");
+var key = Environment.GetEnvironmentVariable("SUPABASE_KEY");
+var options = new Supabase.SupabaseOptions
 {
-    var connStr = builder.Configuration.GetConnectionString("DefaultConnection");
-    try
-    {
-        using var conn = new SqlConnection(connStr);
-        await conn.OpenAsync();
-        return Results.Ok("DB connection successful!");
-    }
-    catch (Exception ex)
-    {
-        return Results.Problem(ex.Message);
-    }
+    AutoConnectRealtime = true
+};
+var supabase = new Supabase.Client(url, key, options);
+await supabase.InitializeAsync();
+
+app.MapGet("/", async () =>
+{
+    var result = await supabase
+        .From<CategoriePublication>()
+        .Get();
+
+    return $"Count: {result.Models.Count} | Raw: {result.Content}";
 });
+
 
 app.UseStaticFiles();
 app.MapFallbackToFile("index.html");
 app.Run();
+
+[Table("categorie_publication")]
+class CategoriePublication : BaseModel
+{
+    [PrimaryKey("id_categorie_publication")]
+    public string? Id { get; set; }
+    [Column("nom")]
+    public string? nom { get; set; }
+
+};
