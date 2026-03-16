e32e4d434c0df7ba82899c0007a1364de366f259
Yeo <yeosuwacos@gmail.com>
2026-03-16 12:52:32 -0400

    [Web app] Post creation form operational

--- Application-web/src/components/subcomponents/CreatePost.jsx
+++ Application-web/src/components/subcomponents/CreatePost.jsx
@@ -1,21 +1,34 @@
 /** Style imports */
+import { useState } from 'react';
 import '../.././assets/CreatePost.css'
 
 /** Create post popup */
 export default function CreatePost(){
+    /** Form variable use states */
+    const [title, setTitle] = useState("");
+    const [textContent, setTextContent] = useState("");
+
+    /** Form submission */
+    const submit = (event) => {
+        event.preventDefault();
+    }; 
+    const handleTitle = (event) => {setTitle(event.target.value);};
+    const handleTextContent = (event) => {setTextContent(event.target.value);};
+
   return(
     <div id="form-container">
         <h2>New post</h2>
-        <form>
+        <form id="create-post-form" onSubmit={submit}>
             {/** Post details */}
-            <input id="form-title" placeholder="Title"></input>
-            <textarea id="form-text" placeholder="Add text" rows={12}></textarea>
+            <input id="form-title" placeholder="Title" value={title} onChange={handleTitle}></input>
+            <textarea id="form-text" placeholder="Add text" rows={12} 
+            value={textContent} onChange={handleTextContent}></textarea>
 
             {/** Confirmation buttons 
              * In either case the popup closes once you press them
              * If the form does not have a title or text at minimum the post submission fails
             */}
-            <button id="form-post" type="button">
+            <button id="form-post" type="submit">
                 <p id="form-post-text">Post</p>
             </button>
             <button id="form-cancel" type="button">
