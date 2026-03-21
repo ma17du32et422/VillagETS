59eaeba0f0d4cbfdfbdfa58910083aff66c67834
DrunkAxolotl <aresgabrielyan@gmail.com>
2026-03-21 19:05:39 -0400

    Web Added Profile Modification Page Layout Skeleton

--- Application-web/src/pages/ProfilePage.jsx
+++ Application-web/src/pages/ProfilePage.jsx
@@ -1,9 +1,48 @@
 function ProfilePage(){
   return(
+      // If not logged in, the user can log in using a form.
+      // If logged in, the user can modify his username and profile picture.
+      // TO BE HANDLED DYNAMICALLY LATER
+      // TODO: CHECK IF THE ATTRIBUTES ARE FORMATTED PROPERLY FOR REACT
     <>
-      <div>
-        <h1>Profile</h1>
-      </div>
+        <div className='profile'>
+            <h1>Profile</h1>
+            <h2>Log In</h2>
+            <form>
+                <div>
+                    <label htmlFor="username">Username: </label>
+                    <input type="text" id="username" name="username"/>
+                </div>
+                <div>
+                    <label htmlFor="password">Password: </label>
+                    <input type="password" id="password" name="password"/>
+                </div>
+            </form>
+            <button>Log In</button>
+            <h2>Modify Details</h2>
+            <button>Modify User</button>
+            <form>
+                <div>
+                    <label htmlFor="newUsername">New Username: </label>
+                    <input type="text" id="newUsername" name="newUsername"/>
+                </div>
+            </form>
+            <button>Apply New User</button>
+            <button>Modify Password</button>
+            <form>
+                <div>
+                    <label htmlFor="oldPassword">Old Password: </label>
+                    <input type="password" id="oldPassword" name="oldPassword"/>
+                </div>
+                <div>
+                    <label htmlFor="newPassword">New Password: </label>
+                    <input type="password" id="newPassword" name="newPassword"/>
+                </div>
+            </form>
+            <button>Apply New Password</button>
+            <button>Modify Profile Picture</button>
+            <image>Profile Picture Above</image>
+        </div>
     </>
   );
 }
