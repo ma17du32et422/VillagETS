4f57eaddeffd8cd0797654c42e3edad9b2a90456
DrunkAxolotl <aresgabrielyan@gmail.com>
2026-03-22 18:47:33 -0400

    Web Splitting of ProfilePage.jsx into multiple components and cleanup

--- Application-web/src/pages/ProfilePage.jsx
+++ Application-web/src/pages/ProfilePage.jsx
@@ -1,48 +1,48 @@
+/** Component imports */
+import LoginForm from "../components/LoginForm";
+import UsernameForm from "../components/UsernameForm";
+import PasswordForm from "../components/PasswordForm";
+
+/** Styling */
+import '../assets/ProfilePage.css'
+
+
+/** If not logged in, the user can log in using a form.
+If logged in, the user can modify his username and profile picture.
+TO BE HANDLED DYNAMICALLY LATER -> PLUG INTO JS LOGIC AND SETUP STATES TO SHOW/HIDE COMPONENTS*/
+
+/** ProfilePage */
 function ProfilePage(){
   return(
-      // If not logged in, the user can log in using a form.
-      // If logged in, the user can modify his username and profile picture.
-      // TO BE HANDLED DYNAMICALLY LATER
-      // TODO: CHECK IF THE ATTRIBUTES ARE FORMATTED PROPERLY FOR REACT
     <>
-        <div className='profile'>
+        <header id="header">
             <h1>Profile</h1>
-            <h2>Log In</h2>
-            <form>
-                <div>
-                    <label htmlFor="username">Username: </label>
-                    <input type="text" id="username" name="username"/>
+        </header>
+
+        <main id="main">
+
+            {/** LoginForm */}
+            <section id="login-container">
+                <div id="login">
+                    <LoginForm />
                 </div>
-                <div>
-                    <label htmlFor="password">Password: </label>
-                    <input type="password" id="password" name="password"/>
+            </section>
+
+            {/** UsernameForm and PasswordForm */}
+            <section id="profile-edit-container">
+                <div id="profile-edit">
+                    <UsernameForm />
+                    <PasswordForm />
+
+                    {/** Profile Picture Selection */}
+                    <div id="profile-picture">
+                        <h3>Profile Picture</h3>
+                        <img alt="Profile Picture"/>
+                        <button>Change Profile Picture</button>
+                    </div>
                 </div>
-            </form>
-            <button>Log In</button>
-            <h2>Modify Details</h2>
-            <button>Modify User</button>
-            <form>
-                <div>
-                    <label htmlFor="newUsername">New Username: </label>
-                    <input type="text" id="newUsername" name="newUsername"/>
-                </div>
-            </form>
-            <button>Apply New User</button>
-            <button>Modify Password</button>
-            <form>
-                <div>
-                    <label htmlFor="oldPassword">Old Password: </label>
-                    <input type="password" id="oldPassword" name="oldPassword"/>
-                </div>
-                <div>
-                    <label htmlFor="newPassword">New Password: </label>
-                    <input type="password" id="newPassword" name="newPassword"/>
-                </div>
-            </form>
-            <button>Apply New Password</button>
-            <button>Modify Profile Picture</button>
-            <image>Profile Picture Above</image>
-        </div>
+            </section>
+        </main>
     </>
   );
 }
