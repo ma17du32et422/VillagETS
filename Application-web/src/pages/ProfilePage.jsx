9580d4a17e7ed24e18d11819d1a11e4d8ea72fdb
Charles <lesagecharles.secondaire@gmail.com>
2026-03-30 15:20:15 -0400

    Update ProfilePage.jsx

--- Application-web/src/pages/ProfilePage.jsx
+++ Application-web/src/pages/ProfilePage.jsx
@@ -52,7 +52,7 @@ function ProfilePage() {
                                     const formData = new FormData();
                                     formData.append('file', file);
 
-                                    const response = await fetch('http://localhost:5000/upload', {
+                                    const response = await fetch('https://apivillagets.lesageserveur.com/upload', {
                                         method: 'POST',
                                         body: formData,
                                     });
