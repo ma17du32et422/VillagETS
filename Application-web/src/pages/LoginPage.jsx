bfe78c909525a2ec8c82d6a7aef64443b6295ac6
Yeosuwacos <yeosuwacos@gmail.com>
2026-04-05 22:30:04 -0400

    [Web] Separated the profile page and the login page

--- Application-web/src/pages/LoginPage.jsx
+++ Application-web/src/pages/LoginPage.jsx
@@ -1,7 +1,30 @@
+/** Component imports */
+import Header from "../components/Header.jsx";
+import LoginForm from "../components/LoginForm";
+import SignupForm from "../components/SignupForm";
+
+/** Styling */
+import '../assets/ProfilePage.css'
+
 function LoginPage(){
-    return(
-        "Hi"
-    )
+    return (
+        <>
+            <header>
+                <Header />
+            </header>
+
+            <main id="profileMain">
+
+                {/** LoginForm and SignupForm */}
+                <section id="login-container">
+                    <div id="login">
+                        <LoginForm />
+                        <SignupForm />
+                    </div>
+                </section>
+            </main>
+        </>
+    );
 }
 
 export default LoginPage;
